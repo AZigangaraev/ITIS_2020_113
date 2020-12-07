7ca6bb8e4eb018403e31b6734a52aa76f243ef4d
StDIsmas <62221545+StDIsmas@users.noreply.github.com>
2020-12-07 06:42:51 -0800

    Iurusov Dima

--- ReqresParser/ReqresParser/Model/UserService.swift
+++ ReqresParser/ReqresParser/Model/UserService.swift
@@ -86,4 +86,47 @@ class UserService {
         }
         dataTask.resume()
     }
+    func loadUser(id: Int, _ completion: @escaping (Result<UserResponse, UserServiceError>) -> Void) {
+        guard let urlComponents = URLComponents(url: baseUrl.appendingPathComponent("users").appendingPathComponent("\(id)"), resolvingAgainstBaseURL: false)
+        else {
+            return completion(.failure(.urlCreation))
+              }
+        guard let url = urlComponents.url else {
+            return completion(.failure(.urlCreation))
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.setValue("text/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.timeoutInterval = timeoutInterval
+        let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            let result: Result<UserResponse, UserServiceError>
+            defer {
+                self.responseQueue.async {
+                    completion(result)
+                }
+            }
+            
+            if let error = error {
+                return result = .failure(.system(error))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return result = .failure(.nonHttpResponse)
+            }
+            guard (200..<300).contains(httpResponse.statusCode) else {
+                return result = .failure(.statusCode(httpResponse.statusCode))
+            }
+            guard let data = data else {
+                return result = .failure(.noData)
+            }
+            do {
+                let response = try self.decoder.decode(UserResponse.self, from: data)
+                result = .success(response)
+            } catch {
+                print("\nResponse:\n\(String(data: data, encoding: .utf8) ?? "")\n")
+                return result = .failure(.parsing(error))
+            }
+        }
+        dataTask.resume()
+    }
 }
--- ReqresParser/ReqresParser/Presentation/UserViewController.swift
+++ ReqresParser/ReqresParser/Presentation/UserViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class UserViewController: UIViewController {
     var userId: Int?
+    let userService = UserService(responseQueue: .main)
+    var user: User? {
+        didSet {
+            nameLabel.text = "\(String(describing: user!.lastName)) \(String(describing: user!.firstName))"
+            emailLabel.text = user!.email
+            loadImage()
+        }
+    }
 
     @IBOutlet var avatarImageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
@@ -16,5 +24,44 @@ class UserViewController: UIViewController {
 
     static func from(storyboard: UIStoryboard) -> UserViewController {
         storyboard.instantiateViewController(identifier: String(describing: UserViewController.self))
+    }
+    
+    func loadUser()
+    {
+   guard let userId = userId else {
+       return
+   }
+   userService.loadUser(id: userId) { result in
+       switch result {
+       case .success(let user):
+           self.user = user.user
+       case .failure(let error):
+           print(error)
+       }
+       
+   }
+    }
+    
+    override func viewDidLoad()
+    {
+        loadUser()
+    }
+    
+         
+        
+    var dataTask: URLSessionDataTask?
+    
+        func loadImage() {
+        guard let User = user else { return }
+        let url = User.avatar
+        
+        dataTask = URLSession.shared.dataTask(with: url) { data, _, _ in
+            DispatchQueue.main.async {
+                if let data = data, let image = UIImage(data: data) {
+                    self.avatarImageView.image = image
+                }
+            }
+        }
+        dataTask?.resume()
     }
 }
